4450f501e8994edfc5a9e10dd70b74b3838a01e1
Dylan Banta <32310773+DylanBanta@users.noreply.github.com>
2020-05-01 05:59:22 -0400

    Update baseOSInstaller.lua

--- Operating System Files/Base SCP OS/baseOSInstaller.lua
+++ Operating System Files/Base SCP OS/baseOSInstaller.lua
@@ -2,16 +2,29 @@
 
 function baseOSInstaller()
 
-local writeFileData = http.get("https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/utils/writeFile.lua").readAll()
-local gitHubData = http.get("https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/utils/gitHub.lua").readAll()
-local readFileUrl = "https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/utils/readFile.lua"
+--filePaths for gitHub files
+local BaseOSPath = "https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/"
+local utilsPath = BaseOSPath .. "utils/"
 
+--filePaths for utils
+local writeFile = utilsPath .. "writeFile.lua"
+local gitHub = utilsPath .. "gitHub.lua"
+local readFile = utilsPath .. "readFile.lua"
+
+--get utils from gitHub
+local writeFileData = http.get(writeFile).readAll()
+local gitHubData = http.get(gitHub).readAll()
+
+--creates util writeFile.lua in installation computer's ./utils directory
 local h = fs.open("./utils/writeFile.lua", "w")
 h.write(writeFileData)
-h.close()	
+h.close()
 
+--load writeFile.lua into API
 os.loadAPI("./utils/writeFile.lua")
-writeFile.writeFile("./utils", "gitHub", gitHubData)
+
+--creates util gitHub.lua in installation computer's ./utils directory
+writeFile.writeFile("./utils", "gitHub.lua", gitHubData)
 os.loadAPI("./utils/gitHub.lua")
 gitHub.get(readFileUrl, "./utils", "readFile")
 end
