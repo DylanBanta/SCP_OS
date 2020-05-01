d0f8b19bb7865b9cc56e8e552d3410cf60594759
Dylan Banta <32310773+DylanBanta@users.noreply.github.com>
2020-05-01 05:46:34 -0400

    .

--- Operating System Files/Base SCP OS/utils/gitHub.lua
+++ Operating System Files/Base SCP OS/utils/gitHub.lua
@@ -1,7 +1,20 @@
 --gitHub.lua
 
+--lua extension
 function get(url, filePath, fileName)
+	local fn = fileName..".lua"
 	os.loadAPI("./utils/writeFile.lua")
 	local data = http.get(url).readAll()
-	writeFile.writeFile(filePath, fileName..".lua", data)
+	
+	writeFile.writeFile(filePath,fn, data)
 end
+
+--custom extension
+function getExt(url, filePath, fileName, ext)
+	local fn = fileName..ext
+	os.loadAPI("./utils/writeFile.lua")
+	local data = http.get(url).readAll()
+	
+	writeFile.writeFile(filePath,fn, data)
+end
+
