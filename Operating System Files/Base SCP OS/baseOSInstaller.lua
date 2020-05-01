3bd0caa3f11171cc087928c481b8847eff2e3db2
Dylan Banta <32310773+DylanBanta@users.noreply.github.com>
2020-05-01 05:31:03 -0400

    Update baseOSInstaller.lua

--- Operating System Files/Base SCP OS/baseOSInstaller.lua
+++ Operating System Files/Base SCP OS/baseOSInstaller.lua
@@ -3,7 +3,7 @@
 function baseOSInstaller()
 
 local writeFileData = http.get("https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/utils/writeFile.lua").readAll()
-local gitHubData = http.get("https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/gitHub.lua").readAll()
+local gitHubData = http.get("https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/utils/gitHub.lua").readAll()
 local readFileUrl = "https://raw.githubusercontent.com/DylanBanta/SCP_OS/master/Operating%20System%20Files/Base%20SCP%20OS/utils/readFile.lua"
 
 local h = fs.open("./utils/writeFile.lua", "w")
